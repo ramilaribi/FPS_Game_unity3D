c0e240c34fdddc02e77f44a0cb1bbf0a4607d289
ramilaribi <laribi.rami@esprit.tn>
2024-02-04 13:34:19 +0100

    shot on target Mode

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -11,8 +11,15 @@ public class Weapon : MonoBehaviour
     bool allowReset = true;
     public float shootingDelay = 2f;
 
+    //Burst 
+    public int bulletPerBurst = 3;
+    public int burstBulletsLeft ;
+
+    //spread 
+    public float spreadIntensity;
 
 
+    // Bullet
     // Prefab of the bullet to be instantiated
     public GameObject bulletPrefab;
     // Transform representing the spawn point of the bullet
@@ -23,29 +30,109 @@ public class Weapon : MonoBehaviour
     public float bulletPrefabLifeTime = 3f;
 
 
+
+    public enum ShootingMode 
+    {
+        Single,
+        Burst,
+        Auto
+    }
+
+    public ShootingMode currentShootingMode;
+
+    private void Awake()
+    {
+        readyToShoot = true;
+        burstBulletsLeft = bulletPerBurst;
+    }
     void Update()
     {
-        // left mouse click
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+       if (currentShootingMode == ShootingMode.Auto)
         {
+            // holding down left mouse for auto shoot 
+            isShooting = Input.GetKey(KeyCode.Mouse0);
+        }
+       else  if (currentShootingMode == ShootingMode.Single || currentShootingMode == ShootingMode.Burst) 
+        {
+            // clicking left mouse once to shoot 
+            isShooting = Input.GetKeyDown(KeyCode.Mouse0);
+
+        }
+       if (readyToShoot && isShooting)
+        {
+            burstBulletsLeft = bulletPerBurst;
             FireWeapon();
         }
     }
 
+
+
     private void FireWeapon()
-    {   //  Instantiate a new bullet GameObject at the position of the bulletSpawn with no rotation (Quaternion.identity)
+    {
+        readyToShoot = false;
+
+        Vector3 shootingDirection = CalculateDirectionAndSpread().normalized;
+       
+        //  Instantiate a new bullet GameObject at the position of the bulletSpawn with no rotation (Quaternion.identity)
         GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
+        
+        // Poiting the bullet to face to shooting direction
+        bullet.transform.forward = shootingDirection;
         // Shoot the Bullet 
         bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward.normalized * bulletVelocity , ForceMode.Impulse) ;
+       
         // Destroy the bullet after time so that s why we use a couroutine because the code still runing until the delay end 
         StartCoroutine(DestroyBulletAfterTime(bullet,bulletPrefabLifeTime));
 
+        // checking if the shot is done
+        if (allowReset)
+        {
+            Invoke("ResetShot", shootingDelay);
+            allowReset = false;
+        }
+        //burst mode 
+        if(currentShootingMode == ShootingMode.Burst && burstBulletsLeft > 1) // We are ready to shot once before this check 
+        {
+            burstBulletsLeft--;
+            Invoke("FireWeapon",shootingDelay);
+        }
      }
+    private void ResetShot()
+    {
+        readyToShoot = true;
+        allowReset = true;
+    }
+    public Vector3 CalculateDirectionAndSpread()
+    {
+        // shooting from the middle where we are pointing at 
+        Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray , out hit))
+        {
+            targetPoint = hit.point;
+        }
+        else
+        {
+            // Shooting at the air
+            targetPoint = ray.GetPoint(100);
+        }
+        Vector3 direction = targetPoint - bulletSpawn.position;
+
+        float x = UnityEngine.Random.Range(-spreadIntensity,spreadIntensity);
+        float y = UnityEngine.Random.Range(-spreadIntensity, spreadIntensity);
+        // returning the shooting direction and spread
+        return direction + new Vector3(x, y, 0);
+    }
+
+
+
+
     // IEnumerator i used for type couroutine methode
     private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
     {
         yield return new WaitForSeconds(delay);
         Destroy(bullet);
 
-            }
+    }
 }
